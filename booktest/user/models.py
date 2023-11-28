0ccb3b45d13dd09209124fb8f3f04a90962869d2
Pozor <shaginov1b@yandex.ru>
2023-11-28 16:30:57 +0300

    add model user book

---
+++ booktest/user/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+
+class UserBook(models.Model):
+    username = models.CharField(max_length=60)
+    mail = models.EmailField(max_length=70)
+    date = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return f'{self.username}'
